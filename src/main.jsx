2430073842bd9238054a00df4b640bc78045a258
Abhishek Bonigala <92784784+bonigalaabhishek@users.noreply.github.com>
2024-07-24 14:40:36 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter >
+    <BrowserRouter basename="/newportfolio">
     <App />
     </BrowserRouter>
   </React.StrictMode>,
